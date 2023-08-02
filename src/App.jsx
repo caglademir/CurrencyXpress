2f57989a75c3031004f1e557a3527cfa62ea8ee6
caglademir <caglllademir@gmail.com>
2023-08-02 21:39:10 +0300

    data pulled

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
 
+import React, { useState, useEffect } from 'react';
 
 
 
 function App() {
 
+  let [currency, setCurrency] = useState(null)
+
+  useEffect(() => {
+    fetch("https://api.exchangerate.host/latest")
+      .then(res => res.json())
+      .then(data => setCurrency(Object.keys(data.rates)))
+  }, [])
+
+
 
   return (
     <>
-      hello world
+     
     </>
   )
 }
