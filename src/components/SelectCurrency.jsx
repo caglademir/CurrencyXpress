d9499e0c25e5d37d88830efb8beda649ed3c7bc7
caglademir <caglllademir@gmail.com>
2023-08-03 15:37:27 +0300

    added CSS

--- src/components/SelectCurrency.jsx
+++ src/components/SelectCurrency.jsx
@@ -3,12 +3,15 @@ import React from 'react'
 export default function SelectCurrency({currency, onChange, label}) {
   return (
     <>
-    <select name={label} onChange={onChange}>
+<div className='flex gap-4 text-lg font-medium text-white font-sans '>
+    <label >{label}</label>
+    <select className='w-3/5 bg-stone-700 ' name={label} onChange={onChange}>
           <optgroup label={label}></optgroup>
           {currency?.map((data, i) => (
             <option value={data} key={i}>{data}</option>
           ))}
         </select>
+</div>
     </>
   )
 }
