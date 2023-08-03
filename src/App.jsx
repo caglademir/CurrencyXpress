1bfd9b0ae3655fb607f3c31c9f6c4c2bbde7e298
caglademir <caglllademir@gmail.com>
2023-08-03 12:02:52 +0300

    request sent, added onClick

--- src/App.jsx
+++ src/App.jsx
@@ -13,28 +13,43 @@ function App() {
       .then(data => setCurrency(Object.keys(data.rates)))
   }, [])
 
+  const [input, setInput] = useState(null)
   const [selectFrom, setSelectFrom] = useState(null)
+  const [selectTo, setSelectTo] = useState(null)
+  const [rate, setRate] = useState({})
+
+  const sendRequest = (e) => {
+
+      fetch(`https://api.exchangerate.host/latest?BASE=${selectFrom}&symbols=${selectTo}&amount=${input}`)
+        .then(res => res.json())
+        .then(data => setRate(data.rates))
+        // .catch(err => console.log(err))
+        // console.log(rate)
+  }
+  let fixedRate = rate[selectTo];
+  
   return (
     <>
       <form >
-        <label onChange={event => setSelectFrom(event.target.value)}>
-          Amount: <input name="myInput" />
+        <label>
+          Amount: <input name="myInput" onChange={e => setInput(e.target.value)} />
         </label>
-        <select name="from" id="">
+        <select name="from" id="" onChange={e => setSelectFrom(e.target.value)}>
           <optgroup label="FROM"></optgroup>
           {currency?.map((data, i) => (
             <option value={data} key={i}>{data}</option>
           ))}
         </select>
         <label>To:</label>
-        <select name="to" id="">
+        <select name="to" id="" onChange={e => setSelectTo(e.target.value)}>
           <optgroup label="TO"></optgroup>
           {currency?.map((data, i) => (
             <option value={data} key={i}>{data}</option>
           ))}
         </select>
-        <button>Convert</button>
+        <button type="button" onClick={sendRequest}>Convert</button>
       </form>
+     <div>{fixedRate?.toFixed(2)}</div>
     </>
   )
 }
